e3beafaf5a250059a4dbdd4ff87c11c2b404471f
Nikolai <ne360cosmos@gmail.com>
2025-10-03 16:50:33 +0300

    Написан тестовый код для бота

--- PinterestTelegramBot/PinterestTelegramBot/Program.cs
+++ PinterestTelegramBot/PinterestTelegramBot/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DotNetEnv;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace PinterestTelegramBot
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        private const string EnvPath = "../../../.env";
+        private const string TelegramBotToken = "TELEGRAM_BOT_TOKEN";
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Env.Load(EnvPath);
+            
+            string token = Environment.GetEnvironmentVariable(TelegramBotToken);
+
+            if (token == null)
+            {
+                Console.WriteLine($"Environment variable [{TelegramBotToken}] not found.");
+                
+                return;
+            }
+            
+            TelegramBotClient bot = new TelegramBotClient(token);
+            User user = await bot.GetMe();
+            
+            Console.WriteLine($"Hello, World! I am user {user.Id} and my name is {user.FirstName}.");
         }
     }
 }
